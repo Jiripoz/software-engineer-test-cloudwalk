ece45acdf51ff20f53690c2dbe16a6c175fed99f
alan <alan.franco.7@gmail.com>
2024-08-05 01:32:43 -0300

    Changed game.player type to Hashset, also included add_player method to our Game Service. This commit also includes the implementation to these function and the respective unit tests

--- src/services/game_service.rs
+++ src/services/game_service.rs
@@ -1,6 +1,43 @@
-use crate::models::Kill;
+use std::collections::{HashMap, HashSet};
+use crate::models::{Game, Kill};
+
 
 pub trait GameService {
     fn new() -> Self;
     fn add_kill(&mut self, kill: Kill);
+    fn add_player(&mut self, player: String);
+}
+
+impl GameService for Game {
+    fn new() -> Self {
+        Game {
+            total_kills: 0,
+            players: HashSet::new(),
+            kills: HashMap::new(),
+            kills_by_means: HashMap::new(),
+        }
+    }
+
+    fn add_kill(&mut self, kill: Kill) {
+        self.total_kills += 1;
+
+        self.add_player(kill.victim.clone());
+        if kill.killer != "<world>" {
+            self.add_player(kill.killer.clone());
+        }
+
+        if kill.killer == "<world>" {
+            *self.kills.entry(kill.victim).or_insert(0) -= 1;
+        } else {
+            *self.kills.entry(kill.killer).or_insert(0) += 1;
+        }
+
+        *self.kills_by_means.entry(kill.weapon).or_insert(0) += 1;
+    }
+
+    fn add_player(&mut self, player: String) {
+        if player != "<world>" {
+            self.players.insert(player);
+        }
+    }
 }
