232a29b9d8093dc1a0e7a98e392dc431a53f4bd0
alan <alan.franco.7@gmail.com>
2024-08-05 01:29:11 -0300

    Added implementation and unit test for KillService

--- src/services/kill_service.rs
+++ src/services/kill_service.rs
@@ -1,3 +1,15 @@
+use crate::models::Kill;
+
 pub trait KillService {
     fn new(killer: String, victim: String, weapon: String) -> Self;
+}
+
+impl KillService for Kill {
+    fn new(killer: String, victim: String, weapon: String) -> Self {
+        Kill {
+            killer,
+            victim,
+            weapon,
+        }
+    }
 }
--- src/tests/test_kill_service.rs
+++ src/tests/test_kill_service.rs
@@ -1,18 +1,15 @@
-
+use crate::models::Kill;
+use crate::services::KillService;
 
 #[test]
-#[ignore = "Not implemented"]
 fn test_new_kill() {
-    // test the creation of a kill event
-    // we sho
+    let kill = Kill::new(
+        "Player1".to_string(),
+        "Player2".to_string(),
+        "MOD_RAILGUN".to_string(),
+    );
 
-    // let kill = Kill::new(
-    //     "Player1".to_string(),
-    //     "Player2".to_string(),
-    //     "MOD_RAILGUN".to_string(),
-    // );
-
-    // assert_eq!(kill.killer, "Player1");
-    // assert_eq!(kill.victim, "Player2");
-    // assert_eq!(kill.weapon, "MOD_RAILGUN");
+    assert_eq!(kill.killer, "Player1");
+    assert_eq!(kill.victim, "Player2");
+    assert_eq!(kill.weapon, "MOD_RAILGUN");
 }
